9af13955c8b9a95222d67b19dc1b7cec6479fac7
Josh Glantz-Hucks <jahteo@gmail.com>
2020-08-19 20:56:18 -0700

    formState working properly, handleChanges mostly finished

--- user-onboarding-app/src/components/Form.js
+++ user-onboarding-app/src/components/Form.js
@@ -1,36 +1,83 @@
 import React, { useState } from 'react'
 
 function Form () {
-    const [formState, setFormState] = useState()
+    const [formState, setFormState] = useState({
+        name: "",
+        email: "",
+        password: "",
+        terms: "true",
+        role: ""
+    })
+    const handleChanges = (event) => {
+        const newFormData = {
+            ...formState, [event.target.name]: event.target.value
+        }
+        setFormState(newFormData)
+        console.log(formState)
+    }
+    const formSubmit = () => {
+
+    }
+
 
     return(
-        <form>
+        <form onSubmit={formSubmit}>
             <label htmlfor="name">
                 Name
-                <input id="name" type="text" name="name" />
+                <input
+                    id="name"
+                    type="text"
+                    name="name"
+                    value={formState.name}
+                    onChange={handleChanges}
+                />
             </label>
             <label htmlfor="email">
                 Email
-                <input id="email" type="text" name="email" />
+                <input
+                    id="email"
+                    type="text"
+                    name="email"
+                    value={formState.email}
+                    onChange={handleChanges}
+                />
             </label>
             <label htmlfor="password">
                 Password
-                <input id="password" type="text" name="password" />
+                <input
+                    id="password"
+                    type="text"
+                    name="password"
+                    value={formState.password}
+                    onChange={handleChanges}
+                />
             </label>
             <label htmlfor="terms">
                 Terms of Service
-                <input id="terms" type="checkbox" name="terms" checked="true" />
+                <input
+                    id="terms"
+                    type="checkbox"
+                    name="terms"
+                    checked={formState.terms}
+                    onChange={handleChanges}
+                />
             </label>
             <label htmlfor="role">
                 Role
-                <select id="role" name="role">
+                <select
+                    id="role"
+                    name="role"
+                    value={formState.role}
+                    onChange={handleChanges}
+                >
+                    <option value="init">--Please choose wisely--</option>
                     <option value="dev">Developer Extraordinaire</option>
                     <option value="webUI">Web UI Monkey</option>
                     <option value="backend">Backend Guru</option>
                     <option value="manager">Introvert Wrangler</option>
                 </select>
             </label>
-            <button type="submit">Submit</button>
+            <button type="submit" disabled="true">Submit</button>
         </form>
     )
 
