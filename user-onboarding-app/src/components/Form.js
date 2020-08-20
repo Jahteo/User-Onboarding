19c90ff745e1e23167b936033c98c37dbddac4a8
Josh Glantz-Hucks <jahteo@gmail.com>
2020-08-19 20:33:26 -0700

    inputs built

--- user-onboarding-app/src/components/Form.js
+++ user-onboarding-app/src/components/Form.js
@@ -1,12 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Form () {
-
+    const [formState, setFormState] = useState()
 
     return(
-        <div>
-            Howdy Form
-        </div>
+        <form>
+            <label htmlfor="name">
+                Name
+                <input id="name" type="text" name="name" />
+            </label>
+            <label htmlfor="email">
+                Email
+                <input id="email" type="text" name="email" />
+            </label>
+            <label htmlfor="password">
+                Password
+                <input id="password" type="text" name="password" />
+            </label>
+            <label htmlfor="terms">
+                Terms of Service
+                <input id="terms" type="checkbox" name="terms" checked="true" />
+            </label>
+            <label htmlfor="role">
+                Role
+                <select id="role" name="role">
+                    <option value="dev">Developer Extraordinaire</option>
+                    <option value="webUI">Web UI Monkey</option>
+                    <option value="backend">Backend Guru</option>
+                    <option value="manager">Introvert Wrangler</option>
+                </select>
+            </label>
+            <button type="submit">Submit</button>
+        </form>
     )
 
 }
